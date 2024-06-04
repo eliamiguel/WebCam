bf338dd2dc0c7e8123d851dac636a176935c93ab
Elias Perreira Miguel <eliasperreiramiguel@gmail.com>
2024-06-04 19:06:04 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+
+const video = document.getElementById('video')
+var canvas = document.querySelector('canvas')
+const baixar = document.getElementById('baixar')
+
+const ligarCamera = () =>{
+  navigator.mediaDevices.getUserMedia({video:true})
+          .then(stream =>{
+              video.srcObject = stream;
+              video.play()
+          })
+          .catch(error => {
+            console.log(error)
+          })
+}
+
+ligarCamera()
+
+
+const tirarFoto = ()=>{
+  canvas.height = video.videoHeight;
+  canvas.width = video.videoWidth;   
+
+  let context = canva.getContext('2d')
+   context.drawImage( video, 0, 0)
+
+   botaoBaixarFoto()
+}
+
+const botaoBaixarFoto = ()=>{
+  const link = document.createElement('a')
+   link.download = 'foto.png'
+   link.href = canvas.toDataURL()
+   link.textContent= 'Baixar foto'
+
+   baixar.appendChild(link)
+   baixar.classList.remove('baixar')
+   document.getElementById('button').classList.add('baixar')
+   document.getElementById('outraFoto').classList.remove('baixar')
+   return link 
+}
+
+
+document.getElementById('button').addEventListener('click', tirarFoto)
+
+
+
